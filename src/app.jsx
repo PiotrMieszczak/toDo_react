6b473580c5227626d4adeba137ce5de3539b738e
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-30 13:28:57 +0200

    created fetch() fn

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,37 @@ import ReactDOM from "react-dom";
 document.addEventListener("DOMContentLoaded", function(){
  
 
-    
+    class TodoList extends React.Component{
+            constructor(props){
+                super(props);
+                this.state={
+                    items: []
+                }
+        }
+        
+        fetchData(){
+            const url = "http://localhost:3000/toDo"
+
+            fetch(url)  
+                .then( r=>r.json() )  //return data in json format
+                .then( response =>{
+                    let newItems = this.state.items.slice();
+                    newItems.push(response);
+                    console.log(response);
+                    this.setState({
+                        items: newItems,
+                    })
+                })
+        }
+        componentDidMount(){
+            this.fetchData();
+        
+            
+        }
+            render(){
+                return <div>TEST</div>
+            }
+    }
 
     class App extends React.Component{
         render(){ 
